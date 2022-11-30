c2529527a51d38b658c5b9a557002cc36fe4b6f8
BishalParajulii <parajulibishal00@gmail.com>
2022-11-30 13:53:16 +0545

    Day1 of js

---
+++ day  1/day1.js
@@ -0,0 +1,30 @@
+console.log('hello world');
+console.log(typeof 'Bishal')
+console.log(typeof null)
+var firstname='Bishal'
+var lastname='Parajuli'
+var country='Nepal'
+var city='Bhaktapur'
+var age=20
+var ismarried=true
+console.log(firstname,lastname,country,city,age,ismarried)
+function test(){
+    let a=5;
+    var b=1;
+
+    ++a;
+    console.log(a);
+    console.log(b)
+    document.write(a);
+    for(var i=0;i<10;i++)
+    {
+        console.log(i);
+    }
+    console.log(i);
+}
+test();
+ // let a=10;
+ // var b=0;
+console.log(a);
+console.log(b);
+const g=9.8
