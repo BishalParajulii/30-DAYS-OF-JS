94aa334d8cd84994a9174d713bd64c446ab38d01
BishalParajulii <parajulibishal00@gmail.com>
2022-11-30 22:38:04 +0545

    cleae

---
+++ day-2/day-2.js
@@ -0,0 +1,22 @@
+let nums =[1,2,3];
+document.write(nums[2]);
+console.log(nums[2])
+let numbers=[1,2,3,4,5]
+console.log(nums==numbers)
+const PI=Math.PI
+console.log(PI)
+console.log(Math.max(-2,3,4,5,7,8,5,2,))
+let bishal = '<br>30 Days of js';
+document.write(bishal)
+document.write(bishal.length)
+document.write(bishal.toLowerCase())
+console.log(bishal.substr(10,15))
+console.log(bishal.includes('js'))
+console.log(bishal.split(''))
+ let string ='facebook,google,twiter,Instagram'
+ document.write(string.split(','))
+ document.write(bishal.replace('js','python'))
+ document.write(bishal.charAt(0))
+document.write(bishal.charCodeAt(8))
+document.write(bishal.indexOf('a'))
+document.write(bishal.search('D'))
